6ea4aa028f51c123f7218ab0773432fd519d0a87
Sharjeel-Rauf <147455894+Sharjeel-Rauf@users.noreply.github.com>
2023-10-10 20:58:33 -0400

    Add files via upload

---
+++ Task 5.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Please enter a string:");
+string thestring = Console.ReadLine();
+char[] newstring = thestring.ToCharArray();
+
+for(int i = 0; i<= newstring.Length - 1; i++)
+{
+    Console.WriteLine(newstring[i]);
+}
